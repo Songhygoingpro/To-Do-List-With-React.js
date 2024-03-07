f9dcc463f46620174bed9f0b9efd899ea2744152
Songhygoingpro <123635791+Songhygoingpro@users.noreply.github.com>
2024-03-07 12:43:17 +0700

    again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import To_do_list from './To-Do-App';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center items-center h-[100vh] bg-sky-500 md:bg-sky-500">
+      <To_do_list />
+  
     </div>
   );
 }
